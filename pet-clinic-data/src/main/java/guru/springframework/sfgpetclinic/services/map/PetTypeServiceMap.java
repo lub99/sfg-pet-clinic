3198ddd04c0d00aac6cd71fdd0766512e444d697
lub99 <matej.luburic@gmail.com>
2022-11-08 09:52:23 +0100

    Add missing @Service to PetTypeServiceMap.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -2,7 +2,9 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetTypeServiceMap extends AbstractServiceMap<PetType, Integer> implements PetTypeService {
     @Override
     Integer getNextId() {
