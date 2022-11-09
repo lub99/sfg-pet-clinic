181b64b81372ff1055e9149fb8935808142b9c2e
lub99 <matej.luburic@gmail.com>
2022-11-09 08:41:02 +0100

    Implement owner jpa entities. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,12 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Pet types cannot be too much and
  * I decided to use Integer type for id to create better architecture
  * because id can be of any type (Long, Int, String, etc.).
  * */
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity<Integer>{
+    @Column(name = "name")
     private String name;
+
 
     public String getName() {
         return name;
