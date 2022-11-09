b421d41f548527308f47ddba9a06ad837522d78c
lub99 <matej.luburic@gmail.com>
2022-11-09 11:05:33 +0100

    Update profiles for using jpa service, tested active profile. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile({"default", "map"})
 public class PetServiceMap extends AbstractServiceMap<Pet, Long> implements PetService {
     @Override
     Long getNextId() {
